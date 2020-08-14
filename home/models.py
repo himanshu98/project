e47cb4aa5d599f2a74c9e4a3085f54f926f5c8fa
himanshu.tomar <tomarhimanshu98@gmail.com>
2020-08-15 03:05:59 +0530

    done with integration

--- home/models.py
+++ home/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from datetime import datetime
+from django.utils import timezone
 # Create your models here.
 class Product(models.Model):
     name=models.CharField(max_length=122)
     qty=models.IntegerField(default=0)
     url=models.CharField(max_length=122)
-    time=models.DateField(default=datetime.today())
+    time=models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
