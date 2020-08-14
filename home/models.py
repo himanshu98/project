57597fbf2095cabd096170ddd8a9313fe5fef41c
himanshu.tomar <tomarhimanshu98@gmail.com>
2020-08-15 00:46:32 +0530

    added product table

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=122)
+    qty=models.IntegerField(default=0)
+    url=models.CharField(max_length=122)
+    time=models.DateField(default=datetime.today())
